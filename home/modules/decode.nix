f2ab56e61e6f98d8de03706bdd7f75da309500e3
Mathieu Moalic <matmoa@pm.me>
2024-01-26 15:18:05 +0100

    Fix enc/dec

--- home/modules/decode.nix
+++ home/modules/decode.nix
@@ -1,26 +1,27 @@
 {pkgs, ...}: let
   dec = pkgs.writeShellScriptBin "dec" ''
-      if [ -z \"\$1\" ]; then
-        echo \"o file specified\"
+    if [ -z "$1" ]; then
+      echo "No file specified"
+      return 1
+    fi
+
+    if [ ! -f "$1" ]; then
+        echo "Encrypted file not found: $1"
         return 1
     fi
 
-    if [ ! -f \"\$1\" ]; then
-        echo \"ncrypted file not found: \$1\"
-        return 1
-    fi
-
-    output=\$1_\$RANDOM
-    openssl enc -aes-256-cbc -d -salt -pbkdf2 -iter 100000 -in \"\$1\" -out \"\$output\"
+    output=$1_$RANDOM
+    ${pkgs.openssl}/bin/openssl enc -aes-256-cbc -d -salt -pbkdf2 -iter 100000 -in "$1" -out "$output"
 
     # Create a directory with the same name as the input file (without .encrypted)
-    output_dir=\"\$\{1%.encrypted\}\"
-    mkdir -p \"\$output_dir\"
+    output_dir="''${1%.encrypted}"
+
+    mkdir -p "$output_dir"
 
     # Extract the tarball contents into the newly created directory
-    tar -xzf \"\$output\" -C \"\$output_dir\"
-    \rm \"\$output\"
-    echo \"ecrypted \$1 to \$output_dir\"
+    ${pkgs.gnutar}/bin/tar -xzf "$output" -C "$output_dir"
+    rm "$output"
+    echo "Decrypted $1 to $output_dir"
 
   '';
 in {
--- home/modules/encode.nix
+++ home/modules/encode.nix
@@ -1,19 +1,20 @@
 {pkgs, ...}: let
   enc = pkgs.writeShellScriptBin "enc" ''
-        if [ -z \"$1\" ]; then
-        echo \"o directory specified\"
-        return 1
+    set -e
+    if [ -z "$1" ]; then
+      echo No directory specified
+      return 1
     fi
 
-    if [ ! -d \"$1\" ]; then
-        echo \"irectory not found: $1\"
-        return 1
+    if [ ! -d "$1" ]; then
+      echo Directory not found: "$1"
+      return 1
     fi
 
-    tar -czf \"$1.tar.gz\" -C \"$1\" .
-    openssl enc -aes-256-cbc -salt -pbkdf2 -iter 100000 -in \"$1.tar.gz\" -out \"$1.encrypted\"
-    \rm \"$1.tar.gz\"
-    echo \"ncrypted $1 to $1.encrypted\"
+    ${pkgs.gnutar}/bin/tar -czf "$1.tar.gz" -C "$1" .
+    ${pkgs.openssl}/bin/openssl enc -aes-256-cbc -salt -pbkdf2 -iter 100000 -in "$1.tar.gz" -out "$1.encrypted"
+    rm "$1.tar.gz"
+    echo "Encrypted $1 to $1.encrypted"
   '';
 in {
   home.packages = [

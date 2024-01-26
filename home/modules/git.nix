6fdd27ae54a13dcb57e0ce90e004126a304cf411
Mathieu Moalic <matmoa@pm.me>
2024-01-26 12:22:13 +0100

    Add autosetupremote to git config

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -7,6 +7,7 @@
       init = {defaultBranch = "main";};
       credential = {helper = "store --file ~/.config/git/.git-credentials";};
       pull = {rebase = false;};
+      push = {autoSetupRemote = true;};
     };
   };
 }

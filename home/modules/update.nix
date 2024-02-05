319d07e1f4b164938823545988a05776cc80b962
Mathieu Moalic <matmoa@pm.me>
2024-02-05 11:23:37 +0100

    Get rid of ~/.zshrc

--- home/modules/update.nix
+++ home/modules/update.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   up = pkgs.writeShellScriptBin "up" ''
-    cd $HOME/nix && git add . && sudo NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake /home/mat/nix#$HOST --option eval-cache false --impure && cd - rm -f ~/.zshenv
+    cd $HOME/nix && git add . && sudo NIXPKGS_ALLOW_UNFREE=1 nixos-rebuild switch --flake /home/mat/nix#$HOST --option eval-cache false --impure && cd -; rm -f ~/.zshenv
   '';
 in {
   home.packages = [

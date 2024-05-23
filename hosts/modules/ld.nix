3cf017df2c13913ca0b7251a1d7d697d86687bcd
Mathieu Moalic <matmoa@pm.me>
2024-05-23 13:40:19 +0200

    d

--- hosts/modules/ld.nix
+++ hosts/modules/ld.nix
@@ -1,3 +1,22 @@
 {pkgs, ...}: {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    # This is the default value
+    libraries = [
+      zlib
+      zstd
+      stdenv.cc.cc
+      curl
+      openssl
+      attr
+      libssh
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+    ];
+  };
 }

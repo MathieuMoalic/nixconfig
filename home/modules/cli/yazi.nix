d78db1486184e3a877265e1ffa17363514ce1bd0
Mathieu Moalic <matmoa@pm.me>
2024-07-04 15:41:19 +0200

    Add yazi wrapper

--- home/modules/cli/yazi.nix
+++ home/modules/cli/yazi.nix
@@ -1,4 +1,19 @@
-{...}: {
+{pkgs, ...}: let
+  script = pkgs.writeShellApplication {
+    name = "yy";
+    text = ''
+      tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+      yazi "$@" --cwd-file="$tmp"
+      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      	cd -- "$cwd"
+      fi
+      rm -f -- "$tmp"
+    '';
+  };
+in {
+  home.packages = [
+    script
+  ];
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;

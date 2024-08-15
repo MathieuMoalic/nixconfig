79785c4a8f90f6e57d9fdd69ee0076cb4829b8df
Mathieu Moalic <matmoa@pm.me>
2024-08-15 14:02:07 +0200

    Fix librewolf on hyprland

--- home/modules/desktop/librewolf.nix
+++ home/modules/desktop/librewolf.nix
@@ -4,6 +4,9 @@
     settings = {
       "webgl.disabled" = false;
       "privacy.resistFingerprinting" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.downloads" = false;
+      "identity.fxaccounts.enabled" = true;
     };
   };
 }

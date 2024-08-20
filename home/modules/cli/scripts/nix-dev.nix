254c7523f627a1011b188d6f723a60c597daf027
Mathieu Moalic <matmoa@pm.me>
2024-08-20 09:45:15 +0200

    Add nixos option search

--- home/modules/cli/scripts/nix-dev.nix
+++ home/modules/cli/scripts/nix-dev.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  osConfig,
+  ...
+}: let
   script1 = pkgs.writeShellApplication {
     name = "nr";
     text = ''
@@ -11,9 +15,16 @@
       NIXPKGS_ALLOW_UNFREE=1 nix shell nixpkgs#"$1" --impure
     '';
   };
+  script3 = pkgs.writeShellApplication {
+    name = "nx";
+    text = ''
+      NIXOS_CONFIG=/home/mat/nix#${osConfig.networking.hostName} nix run github:water-sucks/nixos/0.11.0 -- option -i
+    '';
+  };
 in {
   home.packages = [
     script1
     script2
+    script3
   ];
 }

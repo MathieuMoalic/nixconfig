07481803d3cdeaf57842d2dfd4a26545c522deca
Mathieu Moalic <matmoa@pm.me>
2024-09-05 09:35:05 +0200

    update ssh key

--- hosts/base.nix
+++ hosts/base.nix
@@ -114,8 +114,7 @@
         uid = 1000;
         extraGroups = ["networkmanager" "wheel" "video" "input" "uinput"];
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXS+yVAISHyMWzk+o/jHHMnt9aILZoOFPqe/EkhoDIj"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJH2ihm6q8TDs3UlY7yQqkfzze/l2S4cVlNGLvSpZh6G"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcmHHg1pEOAxvEAyr6p5MY0m3/+BOn8nJOcAf7mMaej"
         ];
         hashedPassword = "$6$4lSS.DgMsihs04VX$uu3991ckntJRdsu/Mo7nYuo06M7s9zXDRT7l110LUjPN4lq1OtUNC1ER/WEaLXCSNBxIiZfMWKc0jdBN.xRs1.";
         shell = pkgs.zsh;

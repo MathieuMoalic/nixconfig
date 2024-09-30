b6a875bbbf3951e80309f02d34b87744c0e33c69
Mathieu Moalic <matmoa@pm.me>
2024-09-30 09:30:15 +0200

    cleanup

--- hosts/modules/restic.nix
+++ hosts/modules/restic.nix
@@ -8,29 +8,16 @@
       owner = config.users.users.mat.name;
     };
   };
+
   environment.systemPackages = with pkgs; [
     restic
   ];
-  services.restic.backups = {
-    podmanBackupLocal = {
-      initialize = true;
-      user = "mat";
-      repository = "/home/mat/backup";
-      paths = ["/home/mat/podman"];
-      passwordFile = config.sops.secrets.restic.path;
-      runCheck = true;
-      timerConfig = {
-        OnCalendar = "daily";
-        Persistent = true;
-      };
-    };
 
-    podmanBackupRemote = {
+  services.restic.backups = let
+    commonSettings = {
       initialize = true;
       user = "mat";
-      repository = "rclone:nyx:z1/backup";
       paths = ["/home/mat/podman"];
-      rcloneConfigFile = /home/mat/.config/rclone/rclone.conf;
       passwordFile = config.sops.secrets.restic.path;
       runCheck = true;
       timerConfig = {
@@ -38,5 +25,18 @@
         Persistent = true;
       };
     };
+  in {
+    podmanBackupLocal =
+      commonSettings
+      // {
+        repository = "/home/mat/backup";
+      };
+
+    podmanBackupRemote =
+      commonSettings
+      // {
+        repository = "rclone:nyx:z1/backup";
+        rcloneConfigFile = /home/mat/.config/rclone/rclone.conf;
+      };
   };
 }

db815dc042e7504f68e14e4548bf5a87304373ba
Mathieu Moalic <matmoa@pm.me>
2024-10-20 17:05:59 +0200

    Add zagreus

---
+++ hosts/zagreus.nix
@@ -0,0 +1,50 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  imports = [
+    ./base.nix
+    ./modules/sddm.nix
+    ./modules/desktop.nix
+  ];
+  programs.steam.enable = true;
+  home-manager.users.mat.imports = [../home/zagreus.nix];
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+
+  networking = {
+    useDHCP = lib.mkDefault true;
+    hostName = "zagreus";
+  };
+
+  boot = {
+    initrd = {
+      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+      kernelModules = [];
+    };
+    kernelModules = ["kvm-amd" "debug"];
+    extraModulePackages = [];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXROOT";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+    };
+  };
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024;
+    }
+  ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  system.stateVersion = "24.05";
+}

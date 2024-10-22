2e196f22aa46321cb5836c9ddcd5292d698db1b7
Mathieu Moalic <matmoa@pm.me>
2024-10-22 19:59:19 +0200

    Gaming zagreus

--- hosts/modules/desktop.nix
+++ hosts/modules/desktop.nix
@@ -17,15 +17,21 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # rtkit is optional but recommended
+  # make pipewire realtime-capable
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
+    # pulse.enable = true;
     #jack.enable = true;
+    lowLatency = {
+      # enable this module
+      enable = true;
+      # defaults (no need to be set unless modified)
+      quantum = 64;
+      rate = 48000;
+    };
   };
   # security.polkit.enable = true;
   systemd = {

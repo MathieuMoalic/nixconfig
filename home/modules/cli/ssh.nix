ccbdfb37807389aceac79929d9cda7c2558d95a2
Mathieu Moalic <matmoa@pm.me>
2024-11-18 16:49:33 +0100

    .

--- home/modules/cli/ssh.nix
+++ home/modules/cli/ssh.nix
@@ -3,6 +3,7 @@
     enable = true;
     serverAliveCountMax = 120;
     serverAliveInterval = 60;
+    extraConfig = "StrictHostKeyChecking accept-new";
     matchBlocks = {
       faculty = {
         hostname = "150.254.111.35";
@@ -16,6 +17,11 @@
         hostname = "matmoa.xyz";
         user = "mat";
         port = 46464;
+      };
+      homeserver-initrd = {
+        hostname = "matmoa.xyz";
+        user = "root";
+        port = 46466;
       };
       nyx = {
         hostname = "nyx.zfns.eu.org";
--- hosts/modules/ld.nix
+++ hosts/modules/ld.nix
@@ -1,8 +1,5 @@
-{pkgs, ...}: {
+{...}: {
   programs.nix-ld = {
     enable = true;
-    # here's a neat way to run nearly any binary by including all of the libraries used by Steam:
-    # contain most of the needed libs for almost all apps
-    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
   };
 }

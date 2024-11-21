a12daac143df99e04fe33e2b29ec35d407c06c41
Mathieu Moalic <matmoa@pm.me>
2024-11-21 10:08:26 +0100

    fix hyprland uwsm

--- hosts/modules/desktop.nix
+++ hosts/modules/desktop.nix
@@ -2,6 +2,7 @@
   programs.hyprland = {
     enable = true;
     xwayland = {enable = true;};
+    withUWSM = true;
   };
   fonts.packages = with pkgs; [
     corefonts # Arial, Times New Roman, etc.

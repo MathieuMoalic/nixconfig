5fa7f03124037870623546f27fbcb70bcfcd85a2
Mathieu Moalic <matmoa@pm.me>
2024-12-05 22:50:40 +0100

    switch to stable

--- hosts/modules/desktop.nix
+++ hosts/modules/desktop.nix
@@ -6,8 +6,9 @@
   };
   fonts.packages = with pkgs; [
     corefonts # Arial, Times New Roman, etc.
-    nerd-fonts.fira-code
-    nerd-fonts.fira-mono
+    # nerd-fonts.fira-code
+    # nerd-fonts.fira-mono
+    (nerdfonts.override {fonts = ["FiraCode" "FiraMono"];}) # FiraMono is for waybar
     roboto
     font-awesome
     source-sans-pro

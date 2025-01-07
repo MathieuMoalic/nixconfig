2fb2b57c33e7bd887d0a28c2ee0474df90b3d5cd
Mathieu Moalic <matmoa@pm.me>
2025-01-07 09:44:45 +0100

    Add window rules for nyx

--- home/nyx.nix
+++ home/nyx.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
@@ -15,6 +16,12 @@
     caddy # web server
     blender # 3d editor
     remmina # rdc
+  ];
+  wayland.windowManager.hyprland.settings.exec-once = lib.mkAfter [
+    "[workspace 1 silent] ${pkgs.vscode}/bin/code"
+    "[workspace 11] ${pkgs.librewolf}/bin/librewolf"
+    "[workspace 21] ${pkgs.foot}/bin/foot"
+    "[workspace 13 silent] ${pkgs.teams-for-linux}/bin/teams-for-linux"
   ];
   home.stateVersion = "23.05";
 }

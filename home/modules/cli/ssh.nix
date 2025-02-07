2e54ea1ec5f998f0346eeb74f57a02cd9b08b589
Mathieu Moalic <matmoa@pm.me>
2025-02-07 20:38:49 +0100

    add carapace

--- home/modules/cli/ssh.nix
+++ home/modules/cli/ssh.nix
@@ -18,6 +18,14 @@
         user = "mat";
         port = 46464;
       };
+      homeserver-vscode = {
+        hostname = "matmoa.xyz";
+        user = "mat";
+        port = 46464;
+        extraOptions = {
+          RemoteCommand = "bash";
+        };
+      };
       homeserver-initrd = {
         hostname = "matmoa.xyz";
         user = "root";
@@ -28,7 +36,7 @@
         user = "mat";
         port = 46464;
       };
-      vscode-nix = {
+      vscode-nyx = {
         hostname = "nyx.zfns.eu.org";
         user = "mat";
         port = 46464;
@@ -41,25 +49,26 @@
         user = "mat";
         port = 46464;
       };
-      prometheus = {
-        hostname = "prometheus.zfns.eu.org";
-        user = "matmoa";
-        port = 28561;
-      };
-      zephyros = {
-        hostname = "zephyros.zfns.eu.org";
-        user = "admin";
-        port = 23099;
-      };
-      eos = {
-        hostname = "eos.zfns.eu.org";
-        user = "matmoa";
-        port = 4545;
+      alecto-vscode = {
+        hostname = "alecto.zfns.eu.org";
+        user = "mat";
+        port = 46464;
+        extraOptions = {
+          RemoteCommand = "bash";
+        };
       };
       zeus = {
         hostname = "zeus.zfns.eu.org";
         user = "mat";
         port = 46464;
+      };
+      zeus-vscode = {
+        hostname = "zeus.zfns.eu.org";
+        user = "mat";
+        port = 46464;
+        extraOptions = {
+          RemoteCommand = "bash";
+        };
       };
       kiosk1 = {
         hostname = "kiosk1.zfns.eu.org";
@@ -69,6 +78,22 @@
         hostname = "kiosk2.zfns.eu.org";
         user = "root";
       };
+      # These 3 are broken
+      # prometheus = {
+      #   hostname = "prometheus.zfns.eu.org";
+      #   user = "matmoa";
+      #   port = 28561;
+      # };
+      # zephyros = {
+      #   hostname = "zephyros.zfns.eu.org";
+      #   user = "admin";
+      #   port = 23099;
+      # };
+      # eos = {
+      #   hostname = "eos.zfns.eu.org";
+      #   user = "matmoa";
+      #   port = 4545;
+      # };
     };
   };
 }

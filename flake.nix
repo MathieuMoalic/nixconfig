79d3775dbd46fea395706535974e9254df266899
Mathieu Moalic <matmoa@pm.me>
2025-03-23 22:57:06 +0100

    Add nvim with n

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     sops-nix.url = "github:Mic92/sops-nix";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.url = "github:notashelf/nvf";
   };
   outputs = {nixpkgs, ...} @ inputs: let
     makeNixosSystem = host:

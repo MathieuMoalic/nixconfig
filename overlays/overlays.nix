3c6b3c624d1b247fb726b27cbd8728e63660c8d3
Mathieu Moalic <matmoa@pm.me>
2025-05-23 14:05:20 +0200

    automatic script import

--- overlays/overlays.nix
+++ overlays/overlays.nix
@@ -1,17 +1,25 @@
-inputs: self: super: {
-  amumax = inputs.amumax.packages.${self.system}.default;
-  nvim-unstable = inputs.nixpkgs_unstable.legacyPackages.${self.system}.neovim-unwrapped;
-  quicktranslate = inputs.quicktranslate.packages.${self.system}.quicktranslate;
-  rose-pine-hyprcursor = inputs.rose-pine-hyprcursor.packages.${self.system}.default;
-  lock = super.callPackage ./scripts/lock.nix {};
-  decode = super.callPackage ./scripts/decode.nix {};
-  encode = super.callPackage ./scripts/encode.nix {};
-  lnmv = super.callPackage ./scripts/lnmv.nix {};
-  nix-run = super.callPackage ./scripts/nix-run.nix {};
-  nix-shell = super.callPackage ./scripts/nix-shell.nix {};
-  power-menu = super.callPackage ./scripts/power-menu.nix {};
-  screenshot-edit = super.callPackage ./scripts/screenshot-edit.nix {};
-  screenshot = super.callPackage ./scripts/screenshot.nix {};
-  update = super.callPackage ./scripts/update.nix {};
-  wireguard-menu = super.callPackage ./scripts/wireguard-menu.nix {};
-}
+inputs: self: super: let
+  lib = super.lib;
+  scriptsPath = ./scripts;
+  scriptPackages = let
+    scriptDirEntries = builtins.readDir scriptsPath;
+  in
+    lib.foldlAttrs (
+      acc: fileName: fileType:
+        if fileType == "regular" && lib.hasSuffix ".nix" fileName
+        then let
+          packageName = lib.removeSuffix ".nix" fileName;
+        in
+          acc // {${packageName} = super.callPackage (scriptsPath + "/${fileName}") {};}
+        else acc
+    )
+    {}
+    scriptDirEntries;
+in
+  {
+    amumax = inputs.amumax.packages.${self.system}.default;
+    nvim-unstable = inputs.nixpkgs_unstable.legacyPackages.${self.system}.neovim-unwrapped;
+    quicktranslate = inputs.quicktranslate.packages.${self.system}.quicktranslate;
+    rose-pine-hyprcursor = inputs.rose-pine-hyprcursor.packages.${self.system}.default;
+  }
+  // scriptPackages

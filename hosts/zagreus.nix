f501b4ad722956130987a4dc8a533c44900798f3
Mathieu Moalic <matmoa@pm.me>
2025-08-13 19:49:15 +0200

    intel wireless drive crash is fixed?

--- hosts/zagreus.nix
+++ hosts/zagreus.nix
@@ -10,17 +10,8 @@
     ./modules/desktop.nix
     ./modules/syncthing.nix
     ./modules/kmonad.nix
+    ./modules/podman.nix
   ];
-  services.sunshine = {
-    enable = true;
-    openFirewall = true;
-    autoStart = true;
-    capSysAdmin = true;
-  };
-  services.udev.extraRules = ''
-    ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1987" ATTR{device}=="0x5013" ATTR{power/wakeup}="disabled"
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="31e3", ATTR{idProduct}=="1312", ATTR{power/wakeup}="disabled"
-  '';
   hardware.wooting.enable = true;
   programs.steam = {
     enable = true;
@@ -63,9 +54,35 @@
       kernelModules = [];
       systemd.enable = true;
     };
-    kernelModules = ["kvm-amd" "debug"];
-    kernelPackages = pkgs.linuxPackages_6_6;
+    kernelModules = [];
+    kernelPackages = pkgs.linuxPackages_testing;
+    extraModprobeConfig = ''
+      options iwlwifi power_save=0 uapsd_disable=1 enable_ini=0
+      options iwlmvm power_scheme=1
+    '';
+    kernelParams = ["pcie_aspm=off"]; # Keeps all PCIe links in the fully active state.
     extraModulePackages = [];
+  };
+  environment.etc."NetworkManager/conf.d/99-wifi-powersave.conf".text = ''
+    [connection]
+    wifi.powersave=2
+  '';
+
+  # Optional but often smoother with Intel:
+  # networking.networkmanager.enable = true;
+  # networking.networkmanager.wifi.backend = "iwd";
+  services = {
+    sunshine = {
+      enable = true;
+      openFirewall = true;
+      autoStart = true;
+      capSysAdmin = true;
+    };
+    udev.extraRules = ''
+      ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="0x1987" ATTR{device}=="0x5013" ATTR{power/wakeup}="disabled"
+      ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="31e3", ATTR{idProduct}=="1312", ATTR{power/wakeup}="disabled"
+    '';
+    # iwd.enable = true;
   };
 
   fileSystems = {

90be47ea972f4d420a7637e1c8f4f645d5d1b0e5
Mathieu Moalic <matmoa@pm.me>
2026-01-24 20:37:00 +0100

    Fix screenshot-edit existing when saving as

--- overlays/scripts/screenshot-edit.nix
+++ overlays/scripts/screenshot-edit.nix
@@ -3,5 +3,5 @@ pkgs.writeShellApplication {
   name = "screenshot-edit";
   runtimeInputs = with pkgs; [grim slurp satty wl-clipboard];
   text = ''
-    grim -g "$(slurp)" -t ppm - | satty --filename - --output-filename "$HOME/dl/screenshot-$(date '+%Y%m%d-%H:%M:%S').png" --initial-tool brush --copy-command wl-copy --early-exit'';
+    grim -g "$(slurp)" -t ppm - | satty --filename - --output-filename "$HOME/dl/screenshot-$(date '+%Y%m%d-%H:%M:%S').png" --initial-tool brush --copy-command wl-copy'';
 }

349ef87b1725efa3fb1b63e943903b76c87c8196
Mathieu Moalic <matmoa@pm.me>
2026-03-03 23:17:06 +0100

    Update wlr-which-key commands

--- home/desktop/wlr-which-key.nix
+++ home/desktop/wlr-which-key.nix
@@ -1,5 +1,10 @@
 {
-  flake.homeModules.wlr-which-key = {pkgs, ...}: let
+  flake.homeModules.wlr-which-key = {
+    pkgs,
+    lib,
+    osConfig ? {},
+    ...
+  }: let
     configFile =
       pkgs.writeText "config.yaml"
       (pkgs.lib.generators.toYAML {} {
@@ -17,90 +22,88 @@
         anchor = "center";
         inhibit_compositor_keyboard_shortcuts = true;
 
-        menu = [
-          {
-            key = "s";
-            desc = "Sleep";
-            cmd = "systemctl suspend";
-          }
+        # unused: b e f j k m n p q t u w y
+        menu =
+          [
+            {
+              key = "s";
+              desc = "Sleep";
+              cmd = "${pkgs.systemd}/bin/systemctl suspend";
+            }
 
-          {
-            key = "r";
-            desc = "Reboot";
-            submenu = [
-              {
-                key = "Return";
-                desc = "Are you sure?";
-                cmd = "systemctl reboot";
-              }
-            ];
-          }
+            {
+              key = "r";
+              desc = "Reboot";
+              submenu = [
+                {
+                  key = "Return";
+                  desc = "Are you sure?";
+                  cmd = "${pkgs.systemd}/bin/systemctl reboot";
+                }
+              ];
+            }
 
-          {
-            key = "o";
-            desc = "Shutdown";
-            submenu = [
-              {
-                key = "Return";
-                desc = "Are you sure?";
-                cmd = "systemctl poweroff";
-              }
-            ];
-          }
+            {
+              key = "o";
+              desc = "Shutdown";
+              submenu = [
+                {
+                  key = "Return";
+                  desc = "Are you sure?";
+                  cmd = "${pkgs.systemd}/bin/systemctl poweroff";
+                }
+              ];
+            }
 
-          {
-            key = "h";
-            desc = "Hibernate";
-            cmd = "systemctl hibernate";
-          }
-          {
-            key = "l";
-            desc = "Lock";
-            cmd = "lock";
-          }
-
-          {
+            {
+              key = "h";
+              desc = "Hibernate";
+              cmd = "${pkgs.systemd}/bin/systemctl hibernate";
+            }
+            {
+              key = "l";
+              desc = "Lock";
+              cmd = "${pkgs.lock}/bin/lock";
+            }
+          ]
+          ++ lib.optional (osConfig.programs.steam.enable or false) {
             key = "z";
             desc = "Steam";
-            cmd = "steam";
+            cmd = "${pkgs.steam}/bin/steam";
           }
-          {
-            key = "x";
-            desc = "Lutris";
-            cmd = "lutris";
-          }
-          {
-            key = "c";
-            desc = "VSCode";
-            cmd = "code";
-          }
-          {
-            key = "d";
-            desc = "Deezer";
-            cmd = "deezer-enhanced";
-          }
+          ++ [
+            {
+              key = "c";
+              desc = "VSCode";
+              cmd = "${pkgs.vscode}/bin/code";
+            }
+            {
+              key = "d";
+              desc = "Deezer";
+              cmd = "${pkgs.deezer-enhanced}/bin/deezer-enhanced";
+            }
 
-          {
-            key = "i";
-            desc = "Inkscape";
-            cmd = "inkscape";
-          }
-          {
-            key = "g";
-            desc = "GIMP";
-            cmd = "gimp";
-          }
-          {
-            key = "v";
-            desc = "LibreOffice";
-            cmd = "libreoffice";
-          }
-          {
-            key = "a";
-            desc = "Anki";
-            cmd = "anki";
-          }
-        ];
+            {
+              key = "i";
+              desc = "Inkscape";
+              cmd = "${pkgs.inkscape}/bin/inkscape";
+            }
+            {
+              key = "g";
+              desc = "GIMP";
+              cmd = "${pkgs.gimp}/bin/gimp";
+            }
+            {
+              key = "v";
+              desc = "LibreOffice";
+              cmd = "${pkgs.libreoffice}/bin/libreoffice";
+            }
+            {
+              key = "a";
+              desc = "Chromium";
+              cmd = "${pkgs.chromium}/bin/chromium";
+            }
+          ];
       });
   in {
     xdg.configFile."wlr-which-key/config.yaml".source = configFile;

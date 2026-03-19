651e4a0ae6480e543c2bcaed166f3ccf35044f5e
Mathieu Moalic <matmoa@pm.me>
2026-03-19 09:25:05 +0100

    Add paperless

---
+++ nixos/selfhosted/paperless.nix
@@ -0,0 +1,88 @@
+{
+  flake.nixosModules.paperless = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: let
+    user = "paperless";
+    url = "paperless.matmoa.eu";
+    port = 10003;
+
+    dataDir = "/var/lib/paperless";
+    mediaDir = "${dataDir}/media";
+    consumeDir = "${dataDir}/consume";
+    exportDir = "${dataDir}/export";
+  in {
+    sops = {
+      secrets."paperless/admin-password" = {
+        owner = user;
+        group = user;
+        mode = "0400";
+      };
+
+      secrets."paperless/secret-key" = {
+        owner = user;
+        group = user;
+        mode = "0400";
+      };
+
+      templates."paperless/environment" = {
+        owner = user;
+        group = user;
+        mode = "0400";
+        content = ''
+          PAPERLESS_SECRET_KEY="${config.sops.placeholder."paperless/secret-key"}"
+        '';
+      };
+    };
+
+    services.paperless = {
+      enable = true;
+      package = pkgs.paperless-ngx;
+
+      user = user;
+
+      address = "127.0.0.1";
+      port = port;
+      domain = url;
+
+      dataDir = dataDir;
+      mediaDir = mediaDir;
+      consumptionDir = consumeDir;
+      consumptionDirIsPublic = false;
+
+      passwordFile = config.sops.secrets."paperless/admin-password".path;
+      environmentFile = config.sops.templates."paperless/environment".path;
+
+      database.createLocally = true;
+
+      configureNginx = false;
+      configureTika = true;
+
+      openMPThreadingWorkaround = true;
+
+      settings = {
+        PAPERLESS_URL = "https://${url}";
+        PAPERLESS_TIME_ZONE = "Europe/Warsaw";
+        PAPERLESS_OCR_LANGUAGE = "eng+pol+fra";
+        PAPERLESS_ACCOUNT_ALLOW_SIGNUPS = false;
+        PAPERLESS_ADMIN_USER = "admin";
+        PAPERLESS_CONSUMER_POLLING = 60;
+      };
+
+      exporter = {
+        enable = true;
+        onCalendar = "daily";
+        directory = exportDir;
+        settings = {
+          compression = false;
+        };
+      };
+    };
+
+    services.caddy.virtualHosts.${url}.extraConfig = ''
+      reverse_proxy 127.0.0.1:${toString port}
+    '';
+  };
+}
--- nixos/selfhosted/self-hosted.nix
+++ nixos/selfhosted/self-hosted.nix
@@ -31,6 +31,7 @@
       watcharr
       blaz
       mont
+      paperless
     ];
   };
 }

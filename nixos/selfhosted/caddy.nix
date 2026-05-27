62f7d7ea7e11b7c6e62e3fdf46e0fea856ee2234
Mathieu Moalic <matmoa@pm.me>
2026-05-27 23:41:27 +0200

    Update caddy defence website

--- nixos/selfhosted/caddy.nix
+++ nixos/selfhosted/caddy.nix
@@ -17,6 +17,27 @@
           root * /srv/public
           file_server browse
         '';
+        "defence.matmoa.eu".extraConfig = ''
+          root * /srv/defence
+          header {
+              Content-Type "application/pdf"
+              Content-Disposition "inline; filename=\"defence.pdf\""
+              X-Content-Type-Options "nosniff"
+          }
+
+          rewrite * /defence.pdf
+          file_server
+        '';
+        "defence-html.matmoa.eu".extraConfig = ''
+          root * /srv/defence
+          header {
+              Content-Disposition "inline; filename=\"defence.html\""
+              X-Content-Type-Options "nosniff"
+          }
+
+          rewrite * /defence.html
+          file_server
+        '';
         "router.matmoa.eu".extraConfig = ''
           import authelia
           reverse_proxy 192.168.1.1

6fe3bff9a158afa79aa47bb425eedeca3ab6559d
Mathieu Moalic <matmoa@pm.me>
2026-06-16 20:53:39 +0200

    Fix ollama

--- nixos/ollama.nix
+++ nixos/ollama.nix
@@ -1,8 +1,28 @@
 {
-  flake.nixosModules.ollama = {pkgs, ...}: let
+  flake.nixosModules.ollama = {
+    config,
+    pkgs,
+    ...
+  }: let
     chatDomain = "chat.matmoa.eu";
     llmDomain = "llm.matmoa.eu";
   in {
+    sops = {
+      secrets."ollama/llm_api_token" = {
+        owner = "caddy";
+        group = "caddy";
+        mode = "0400";
+      };
+      templates."caddy-llm.env" = {
+        owner = "caddy";
+        group = "caddy";
+        mode = "0400";
+        content = ''
+          LLM_API_TOKEN=${config.sops.placeholder."ollama/llm_api_token"}
+        '';
+      };
+    };
+
     networking.firewall.allowedTCPPorts = [80 443];
     services.ollama = {
       enable = true;
@@ -14,6 +34,13 @@
       environmentVariables = {
         CUDA_VISIBLE_DEVICES = "0,1,2,3,4,5,6";
         OLLAMA_MODELS = "/var/lib/ollama/models";
+
+        OLLAMA_CONTEXT_LENGTH = "65536";
+        OLLAMA_NUM_PARALLEL = "1";
+        OLLAMA_MAX_LOADED_MODELS = "1";
+
+        OLLAMA_FLASH_ATTENTION = "1";
+        OLLAMA_KV_CACHE_TYPE = "q8_0";
       };
     };
 
@@ -33,7 +60,7 @@
 
     services.caddy = {
       enable = true;
-
+      environmentFile = config.sops.templates."caddy-llm.env".path;
       virtualHosts.${chatDomain}.extraConfig = ''
         reverse_proxy 127.0.0.1:8080
       '';
@@ -43,6 +70,8 @@
 
         handle @authorized {
           reverse_proxy 127.0.0.1:11434 {
+            header_up Host 127.0.0.1:11434
+
             transport http {
               read_timeout 1800s
             }

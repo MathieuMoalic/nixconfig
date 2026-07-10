77244755676b0b276d813f5948ffbe55f6ef2b11
Mathieu Moalic <matmoa@pm.me>
2026-07-10 16:22:44 +0200

    Update fish aliases

--- home/cli/aliases.nix
+++ home/cli/aliases.nix
@@ -11,9 +11,7 @@
         l = "eza -al --across --icons -s age";
         ll = "eza -algh --across --icons -s age";
         lt = "eza -al --across --icons -s age --tree";
-        e = "nvim";
-        ef = "nvim -- (tv files --no-remote)";
-        eg = "set -l p (string split -m2 ':' -- (tv text --no-remote)); test (count $p) -ge 2; and nvim +$p[2] -- $p[1]";
+        e = "hx";
         op = "xdg-open";
         tldr = "tldr -q";
         myip = "curl ifconfig.me; echo";

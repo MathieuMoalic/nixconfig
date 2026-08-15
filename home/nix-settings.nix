79259da6aa237eabc7c22d2c84f95e65c87c5b76
Mathieu Moalic <matmoa@pm.me>
2026-08-15 11:08:52 +0200

    Fix nix shell warnings

--- home/nix-settings.nix
+++ home/nix-settings.nix
@@ -2,16 +2,10 @@
   flake.homeModules.nixSettings = {...}: {
     nix.settings = {
       experimental-features = "nix-command flakes";
-      auto-optimise-store = true;
-      use-xdg-base-directories = true;
       warn-dirty = false;
       substituters = [
         "https://cache.nixos.org"
         "https://cache.nixos-cuda.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
     };
   };

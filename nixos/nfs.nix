5019872ae4f8e4aa0b8476917fa10124c1de9ad3
Mathieu Moalic <matmoa@pm.me>
2026-08-24 10:48:48 +0200

    More robust backups

--- nixos/nfs.nix
+++ nixos/nfs.nix
@@ -3,22 +3,34 @@
     makeNFS = device: {
       inherit device;
       fsType = "nfs";
+
       options = [
         "nfsvers=4.2"
         "_netdev"
+
         "x-systemd.automount"
         "x-systemd.idle-timeout=1min"
-        "x-systemd.mount-timeout=30s"
+
+        # NAS being unavailable is not a boot failure.
         "nofail"
+
+        # Fail the initial mount attempt reasonably quickly.
+        "x-systemd.mount-timeout=15s"
+
+        # Once successfully mounted, preserve normal hard-NFS
+        # semantics rather than risking silent I/O errors.
         "hard"
-        "timeo=600"
+        "timeo=100"
         "retrans=2"
       ];
     };
   in {
     fileSystems = {
-      "/mnt/nas" = makeNFS "150.254.111.48:/mnt/Primary/zfn/matmoa";
-      "/mnt/nas2" = makeNFS "150.254.111.3:/mnt/zfn2/zfn2/matmoa";
+      "/mnt/nas" =
+        makeNFS "150.254.111.48:/mnt/Primary/zfn/matmoa";
+
+      "/mnt/nas2" =
+        makeNFS "150.254.111.3:/mnt/zfn2/zfn2/matmoa";
     };
   };
 }

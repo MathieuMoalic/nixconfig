71b9427df39f5a1bffef71a6977d654809139ff5
Mathieu Moalic <matmoa@pm.me>
2026-09-25 10:34:35 +0200

    Move polish.html to own repo

--- nixos/selfhosted/polish.nix
+++ nixos/selfhosted/polish.nix
@@ -1,8 +1,11 @@
 {
   flake.nixosModules.polish = {pkgs, ...}: let
-    site = pkgs.writeTextDir "index.html" (
-      builtins.readFile ./polish.html
-    );
+    site = pkgs.fetchFromGitHub {
+      owner = "MathieuMoalic";
+      repo = "polish";
+      rev = "b76d0fbf9a24a628a94c62fbc0a255a4c1f5d1bc";
+      hash = "sha256-oda7ROBBOfMCeZFNWMbG6+BsTP6KnkFdw1dMq5/WDN0=";
+    };
   in {
     services.caddy.virtualHosts."polish.matmoa.eu".extraConfig = ''
       handle /translate {
